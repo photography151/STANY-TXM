06e18535047d93ef3fb91509a84f3d37643d92ba
STANY-TECH <priscajohnson976@gmail.com>
2025-06-01 20:47:30 +0300

    Add files via upload

---
+++ plugins/support.js
@@ -0,0 +1,80 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+cmd({
+    pattern: "support",
+    alias : "version",
+    desc: " allmenu",
+    category: "allmenu",
+    react: "🫅",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+let dec = `    
+⟣──────────────────⟢
+▧ *ᴄʀᴇᴀᴛᴏʀ* : *Stanley technology(🇹🇿)*
+▧ *ᴍᴏᴅᴇ* : *${config.MODE}*
+▧ *ᴘʀᴇғɪx* : *${config.PREFIX}*
+▧ *ʀᴀᴍ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+▧ *ᴠᴇʀsɪᴏɴ* : *V.1* ⚡
+▧ *ᴜᴘᴛɪᴍᴇ* : ${runtime(process.uptime())}
+
+⟣──────────────────⟢
+
+> STANY TCX
+
+⟣──────────────────⟢
+${readMore}
+\`CHANNEL🛠️\`
+https://whatsapp.com/channel/0029Vb5RfcrJ3jv4u0wJwd0r
+
+\`GROUP\` 👥
+
+https://chat.whatsapp.com/FyQdRTWZAyi8xrd2KOHxgr
+
+\`STANY TECH TZ\` *Dev💙❤️*
+
+wa.me/+255750638502?text=Support!
+
+⟣──────────────────⟢
+
+`;
+
+await conn.sendMessage(
+            from,
+            {
+                image: { url: `https://files.catbox.moe/goel5y.jpg` },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: false,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363302677217436@newsletter',
+                        newsletterName: '『 STANY-TECH』',
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+        // Send audio
+        await conn.sendMessage(from, {
+            audio: { url: 'https://files.catbox.moe/a1sh4u.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
