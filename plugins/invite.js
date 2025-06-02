8de942d48ee6884ba3a896977a3152026b96d909
STANY-TECH <priscajohnson976@gmail.com>
2025-06-02 23:10:07 +0300

    Create invite.js

---
+++ plugins/invite.js
@@ -0,0 +1,27 @@
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "invite",
+    react: "🖇️",
+    alias: ["grouplink","glink"],
+    desc: "To Get the Group Invite link",
+    category: "group",
+    use: '.invite',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isCreator ,isDev, isAdmins, reply}) => {
+try{
+const msr = (await fetchJson('https://raw.githubusercontent.com/JawadYTX/KHAN-DATA/refs/heads/main/MSG/mreply.json')).replyMsg
+
+if (!isGroup) return reply(msr.only_gp)
+if (!isAdmins) { if (!isDev) return reply(msr.you_adm),{quoted:mek }} 
+if (!isBotAdmins) return reply(msr.give_adm)
+const code = await conn.groupInviteCode(from)
+
+ await conn.sendMessage(from , { text: `*🖇️ Group Link*\n\nhttps://chat.whatsapp.com/${code}`}, { quoted: mek } )
+} catch (e) {
+await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+console.log(e)
+reply(`❌ *Error Accurated !!*\n\n${e}`)
+}
+} )
