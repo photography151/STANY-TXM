b1cc197f5c8947f112809b05e04115bedff9884c
STANY-TECH <priscajohnson976@gmail.com>
2025-06-03 01:45:56 +0300

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -19,7 +19,7 @@ cmd({
         await reply("🔍 Checking for STANY TXM updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/Stanking11 l/STANY-TXM/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/Stanking11/STANY-TXM/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
@@ -33,7 +33,7 @@ cmd({
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/Stanking/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/Stanking/STANY-TXM/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "GOTAR-XMD-main");
+        const sourcePath = path.join(extractPath, "STANY-TXM-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
